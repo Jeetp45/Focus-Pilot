975a227f1f05aa2ff814c194d7075fa152cf644c
Jeetp45 <141786979+Jeetp45@users.noreply.github.com>
2025-07-08 17:49:24 -0400

    styling, and checking user registration and login

--- focus-pilot/src/components/ui/Topbar.tsx
+++ focus-pilot/src/components/ui/Topbar.tsx
@@ -6,12 +6,17 @@ const Topbar = () => {
   return (
     <header className='flex justify-between items-center p-4 bg-white dark:bg-gray-800 shadow-md'>
       <div className='flex items-center gap-4'>
-        <span className='text-sm text-gray-700 dark:text-gray-300'>
+        <span className='text-xl font-bold font-serif text-gray-700 dark:text-gray-300'>
           Welcome {user?.username}
         </span>
       </div>
       <div>
-        <button onClick={logout}>Logout</button>
+        <button
+          className='w-full bg-blue-600 font-serif hover:bg-blue-700 hover:cursor-pointer text-white text-lg font-semibold py-2 px-4 rounded-md transition'
+          onClick={logout}
+        >
+          Logout
+        </button>
       </div>
     </header>
   );
